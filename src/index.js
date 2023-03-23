983d6f4228436dd590fca7bc3d21c4742e2594d5
Edwin Aloo <edwin.aloo@student.moringaschool.com>
2023-03-23 11:35:32 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
+  const form = document.getElementById("create-task-form");
+form.addEventListener("submit", function(event) {
+  event.preventDefault();
+
+  const input = document.getElementById("new-task-description");
+  const task = input.value;
+
+  const listItem = document.createElement("li");
+  listItem.textContent = task;
+
+  const taskList = document.getElementById("tasks");
+  taskList.appendChild(listItem);
+
+  input.value = ""; // clear the input field
+});
   // your code here
 });
+
+
